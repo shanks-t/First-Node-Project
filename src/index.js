c688483a032b627caa1da24b476cbf02ddfa50f4
shanks-t <shanksleslietrey@gmail.com>
2022-06-04 11:37:25 -0500

    added babel and webpack config which tells babel-loader to include .js files and exclude node_modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import computations from "./computations";
+
 console.log('hello, project.');
+
+// In the example, the ||= will check if the songsCount is false (0).
+// If false, then the right value will be assigned to the left variable.
+let myPlaylist = { songsCount: 0, songs: [] };
+myPlaylist.songsCount ||= 100;
+console.log(myPlaylist); // This will print: {songsCount: 100, songs: Array(0)}
